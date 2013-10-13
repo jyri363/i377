dce41ad1c1f6bff51576fa050435d7fe74af2731
Jüri-Joonas Kerem <jyri363@gmail.com>
2013-10-13 03:47:46 +0300

    T

--- src/HomePage.java
+++ src/HomePage.java
@@ -15,20 +15,19 @@ import javax.servlet.http.HttpSession;
 public class HomePage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// esiteks teeme veidi lihtsamaks.
+
 		PrintWriter out = response.getWriter(); 
 		HttpSession sess = request.getSession();
 		Object param = request.getParameter("param");
-		// teiseks kasutan tuima htmli koodi (tegelkult peaks jsp.-s seda tegema)
+
 		response.setContentType("text/html");
 
 		out.println("<html><head><title>Hello!</title></head><body>Hello<br />");
-		// Nüüd peab mingid sessi jamad lisama, õnneks on selle kohta googlis olemas!
+	
 		out.println("Your session id is " +  sess.getId() + "<br />");
 		sess.setAttribute("param", param);
 		out.println("Session attribute is " + param);	
 		out.println("</body></html>");
-		// Nüüd peaks testima või vähemalt kontrollima, kas asi töötab, kuid kahjuks pole mul praegu aega!!!
 	}
 
 }
--- src/SessionCount.java
+++ src/SessionCount.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpSessionEvent;
  */
 public class SessionCount extends HttpServlet {
 	// kasutan lihtsalt: http://www.mkyong.com/servlet/a-simple-httpsessionlistener-example-active-sessions-counter/ 
-	// ei ole kindel, kas asi töötab!
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().println("<html><head><title>Count</title></head><body>count: "+ SessionCounterListener.getTotalActiveSession()+"</body></html>");
 	}
